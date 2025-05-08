7e34289024458411da9be036d5e9fc24c5f474ab
RND-HW3 <RND-HW3@RND-HW3>
2025-05-09 08:53:10 +0900

    초기 커밋.

---
+++ Finally/MainApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+
+namespace Finally
+{
+    class MainApp
+    {
+        static int Divide(int dividend, int divisor)
+        {
+            try
+            {
+                WriteLine("Divide() Start");
+                return dividend / divisor;                
+            }
+            catch (DivideByZeroException e)
+            {
+                WriteLine("Divide() 예외 발생");
+                throw e;
+            }
+            finally
+            {
+                WriteLine("Divide() End");
+            }            
+        }        
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Write("피연산자 입력: ");
+                String temp = ReadLine();
+                int dividend = Convert.ToInt32(temp);
+
+                Write("제수 입력: ");
+                temp = ReadLine();
+                int divisor = Convert.ToInt32(temp);
+
+                WriteLine($"{dividend} / {divisor} = {Divide(dividend, divisor)}");
+            }
+            catch (FormatException e)
+            {
+                WriteLine($"Error: {e}");
+            }
+
+            catch (DivideByZeroException e)
+            {
+                WriteLine($"error: {e.Message}");
+            }
+            finally
+            {
+                WriteLine("Program End");
+            }
+        }
+    }
+}
